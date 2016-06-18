7f2fa0bf6e228688f63fb0d51d97f970a6f2d0c5
y-salnikov <y.salnikov65535@gmail.com>
2016-06-18 20:22:39 +0400

    add console for character device

---
+++ charcon.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python2
+# -*- encoding=UTF-8 -*-
+
+import termios, sys, os, select, Queue, time
+
+default_device="/dev/kforth"
+
+def main():
+	fd = sys.stdin.fileno()
+	dev= os.open(default_device,os.O_RDWR)
+	old = termios.tcgetattr(fd)
+	new = termios.tcgetattr(fd)
+	new[3]=new[3] & ~termios.ICANON
+	is_readable=[dev,sys.stdin]
+	is_writable=[dev,sys.stdout]
+	is_error=[]
+	inbuf=Queue.Queue()
+	outbuf=Queue.Queue()
+	try:
+		termios.tcsetattr(fd, termios.TCSADRAIN, new)
+		while 1:
+			r,w,e = select.select(is_readable,is_writable,is_error)
+			if sys.stdin in r: inbuf.put_nowait(sys.stdin.read(1))
+			if dev in r:	   outbuf.put_nowait(os.read(dev,1))
+			if ((sys.stdout in w) and ( not outbuf.empty() )): sys.stdout.write(outbuf.get_nowait())
+			if (( dev in w )      and ( not inbuf.empty() ) ): os.write(dev,inbuf.get_nowait())
+			time.sleep(0.01)
+	finally:
+		termios.tcsetattr(fd, termios.TCSADRAIN, old)
+		
+
+main()
